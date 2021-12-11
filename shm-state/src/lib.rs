91b565d69f1d5a6ede7b4c848923e05ef213df15
Andreas Molzer <andreas.molzer@gmx.de>
2021-12-11 17:14:36 +0100

    Finish and add prime generate example

--- shm-state/src/lib.rs
+++ shm-state/src/lib.rs
@@ -1,8 +1,30 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use memfile::{MemFile};
+use std::os::unix::io::RawFd;
+
+pub struct SharedFd {
+    fd: RawFd,
+}
+
+impl SharedFd {
+    /// Import a shared file descriptor based on environment variable `SHM_SHARED_FDS`.
+    ///
+    /// # Safety
+    /// Caller asserts that the environment variable has been set to a file descriptor that is not
+    /// owned by any other resource.
+    pub unsafe fn from_env() -> Option<Self> {
+        let var = std::env::var_os("SHM_SHARED_FDS")?;
+        let num = var.to_str()?.split(',').next()?;
+        let fd: i32 = num.parse().ok()?;
+        Some(SharedFd { fd: RawFd::from(fd) })
+    }
+
+    /// Open the file descriptor.
+    ///
+    /// This can fail if for some reason the file descriptor does not refer to an anonymous memory
+    /// file.
+    pub fn into_file(self) -> Result<MemFile, std::io::Error> {
+        // It's not necessary to preserve the file descriptor here.
+        // It can be restored in any case.
+        MemFile::from_file(self.fd).map_err(|err| err.into_error())
     }
 }
--- shm-watch/src/main.rs
+++ shm-watch/src/main.rs
@@ -1,3 +1,33 @@
+use std::process::Command;
+use std::os::unix::{io::AsRawFd, process::CommandExt};
+use memfile::MemFile;
+
 fn main() {
-    println!("Hello, world!");
+    let file = MemFile::create_sealable("persistent")
+        .expect("failed to initialized shm-file");
+    // Just reserve a file descriptor...
+    let placeholder = MemFile::create_default("placeholder")
+        .expect("failed to initialized shm-file");
+    let rawfd = file.as_raw_fd();
+    let target = placeholder.as_raw_fd();
+
+    let mut args = std::env::args_os().skip(1);
+    let cmd = args.next().expect("no given");
+    let args: Vec<_> = args.collect();
+
+    let mut proc = Command::new(&cmd);
+    proc.args(&args);
+    proc.env("SHM_SHARED_FDS", format!("{}", target));
+
+    unsafe {
+        proc.pre_exec(move || {
+            if -1 == libc::dup2(rawfd, target) {
+                panic!("Failed to dup file descriptor pre-exec");
+            } else {
+                Ok(())
+            }
+        });
+    }
+
+    let _ = proc.exec();
 }
