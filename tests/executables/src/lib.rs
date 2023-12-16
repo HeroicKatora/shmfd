d7f2b80dfed2e26f576a88c643df3bb494382e06
Andreas Molzer <andreas.molzer@gmx.de>
2023-12-16 03:05:29 +0100

    Add text fixture for command success

--- tests/executables/src/lib.rs
+++ tests/executables/src/lib.rs
@@ -1,3 +1,5 @@
 pub const SHM_FD: &str = env!("CARGO_BIN_FILE_SHM_FD_shm-fd");
 
 pub const SHM_RESTORE: &str = env!("CARGO_BIN_FILE_SHM_SNAPSHOT_shm-restore");
+
+pub const SHM_PRIMES_SNAPSHOT: &str = env!("CARGO_BIN_FILE_PRIMES_SNAPSHOT_primes-snapshot");
--- tests/restore-v1/src/main.rs
+++ tests/restore-v1/src/main.rs
@@ -1,1 +1,16 @@
+use shmfd_test_validate::Env;
+use shmfd_test_executables::SHM_PRIMES_SNAPSHOT;
+
+use std::process::Command;
+
 fn main() {}
+
+#[test]
+fn primes_snapshot() {
+    let env = Env::new();
+    env.shared_fd({
+        let mut cmd = Command::new(SHM_PRIMES_SNAPSHOT);
+        cmd.args(["1", "10000"]);
+        cmd
+    }).success();
+}
